31be658b9412b47cc4c6371a665a9d120f6f6f02
mohamedwys <ebookmwl@gmail.com>
2026-01-19 11:34:59 +0400

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
-// pages/_app.tsx
 import type { AppProps } from "next/app";
 import { AppProvider } from "@shopify/polaris";
 import AppBridgeProvider from "../components/providers/AppBridgeProvider";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import "@shopify/polaris/build/esm/styles.css";
-import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider i18n={enTranslations}>
-      <AppBridgeProvider>
+    <AppBridgeProvider>
+      <AppProvider i18n={enTranslations}>
         <Component {...pageProps} />
-      </AppBridgeProvider>
-    </AppProvider>
+      </AppProvider>
+    </AppBridgeProvider>
   );
 }
